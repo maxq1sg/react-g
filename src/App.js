de23d4399212d666c15de21c424a25f69ab9e8f4
maxq1seg <maximb00131516@gmail.com>
2021-05-28 10:10:17 +0300

    deleted changes navbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from "react";
 import Content from "./Content";
 import Footer from "./Footer";
 import Header from "./Header";
-import Navbar from "./Navbar";
 
 const App = () => {
   return (
     <div>
-      we are an amazing company
-      <Navbar />
+      we are an amazing company 
       <Header />
       <Content />
       <Footer />
