4e8d8bf4f427ba10b571d3d0100a63df97c0eef7
maxq1seg <maximb00131516@gmail.com>
2021-05-28 09:54:41 +0300

    Revert "added list to thecontent"
    This reverts commit e141dbc2ac8de35e363bff7036bb8e6019f0837f.

--- src/Content.jsx
+++ src/Content.jsx
@@ -9,11 +9,6 @@ const Content = () => {
       <p>ya lubly kushat</p>
       <p>ya lubly kushat</p>
       <p>ya lubly kushat</p>
-      <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
     </div>
   );
 };
