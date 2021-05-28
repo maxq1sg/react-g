9fce962a59caa51ef7b3e3d413cb8086c6980715
maxq1seg <maximb00131516@gmail.com>
2021-05-28 11:42:00 +0300

    sc wrapper ontetn

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-
+import styled from "styled-components";
+const ContentWrapper = styled.div``;
 const Content = () => {
   return (
-    <div>
+    <ContentWrapper>
       <p>ya lubly kushat</p>
       <p>ya lubly kushat</p>
       <p>ya lubly kushat</p>
       <p>ya lubly kushat</p>
       <p>ya lubly kushat</p>
       <p>ya lubly kushat</p>
-    </div>
+    </ContentWrapper>
   );
 };
 
