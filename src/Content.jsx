3f16aefdf909c0355e7a71a8c61a318dfcd89105
maxq1seg <maximb00131516@gmail.com>
2021-05-28 08:38:46 +0300

    modif content in detached

--- src/Content.jsx
+++ src/Content.jsx
@@ -4,6 +4,11 @@ const Content = () => {
   return (
     <div>
       <p>ya lubly kushat</p>
+      <p>ya lubly kushat</p>
+      <p>ya lubly kushat</p>
+      <p>ya lubly kushat</p>
+      <p>ya lubly kushat</p>
+      <p>ya lubly kushat</p>
     </div>
   );
 };
