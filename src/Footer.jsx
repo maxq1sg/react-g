89310127b638064e2b02116f82e678aa2d16c29c
maxq1seg <maximb00131516@gmail.com>
2021-05-28 09:33:10 +0300

    modified footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div>
-            copyright
-        </div>
-    )
-}
+  return <div>copyright maxima bernlaksn</div>;
+};
 
-export default Footer
+export default Footer;
