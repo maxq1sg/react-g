01b161cfc9b78a1a2707b98f22ef29c08b48cc9f
maxq1seg <maximb00131516@gmail.com>
2021-05-28 10:08:47 +0300

    added navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Content from "./Content";
 import Footer from "./Footer";
 import Header from "./Header";
+import Navbar from "./Navbar";
 
 const App = () => {
   return (
     <div>
-      we are an amazing company 
+      we are an amazing company
+      <Navbar />
       <Header />
       <Content />
       <Footer />
